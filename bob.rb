55e792ba22498c7ae3c44138f086f07e5492c226
Radek <radoslaw.zawada@starware.com.pl>
2018-03-08 08:53:52 +0100

    Add sentence class

--- bob.rb
+++ bob.rb
@@ -1,6 +1,19 @@
 class Bob
-  def self.hey(sentence)
-    return "Whoa, chill out!" if sentence[-1] == "!" || sentence.upcase == sentence
+  def self.hey(phrase)
+    sentence = Sentence.new phrase
+    return "Whoa, chill out!" if sentence.is_yell?
     "Whatever."
+  end
+end
+
+class Sentence
+  attr_reader :sentence
+
+  def initialize(sentence)
+    @sentence = sentence
+  end
+
+  def is_yell?
+    sentence[-1] == "!" || sentence.upcase == sentence
   end
 end
